665e7499075620edf8aea83caed547b47e19726b
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-12 03:40:01 -0500

    Added contact me

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,4 +1,9 @@
 function Contact() {
-  return <h1> Contact</h1>;
+  return (
+    <div>
+      <h1> Contact Me</h1>
+      <p className="mw-50">Email: dup1040@gmail.com</p>
+    </div>
+  );
 }
 export default Contact;
