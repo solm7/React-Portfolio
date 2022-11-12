7bfb7865dde4e40b40554affcb959ce383b2a145
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-12 02:38:16 -0500

    Added Codemonsters

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -1,4 +1,39 @@
+import CodeMonsters from "../images/codemonsters.png";
+// import MyDiary from "../images/mydiary.png";
+// import PassWordGen from "../images/passwordgenerator.png";
+// import weatherApi from "../images/weatherapi.png";
+// import WorkDay from "../workdayscheduler.png";
+
 function Portfolio() {
-  return <h1> Portfolio</h1>;
+  return (
+    <div>
+      <h1> Portfolio</h1>
+      <div className="portfolio-container">
+        <div className="card w-25">
+          <img
+            src={CodeMonsters}
+            className="card-img-top"
+            alt="Yellow monster with one eye"
+          />
+          <div className="card-body d-flex flex-column justify-content-between">
+            <h5 className="card-title fs-2">CodeMonsters</h5>
+            <p className="card-text">"A website to connect with Developers"</p>
+            <div className="button-box">
+              <a href="https://github.com/jdanleano/Code-Monsters">
+                <button type="button" className="btn btn-primary mb-1">
+                  GitHub Repository link
+                </button>
+              </a>
+              <a href="https://jdanleano.github.io/Code-Monsters/">
+                <button type="button" className="btn btn-primary">
+                  Deployment to application
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Portfolio;
