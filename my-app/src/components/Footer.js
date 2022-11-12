f5c9d2489060709ab6cd68ab6e2621ad0c713747
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-12 03:31:14 -0500

    Added Resume

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,4 +1,15 @@
-function Footer() {
-  return <h1> Footer</h1>;
+function Footer(props) {
+  return (
+    <footer>
+      <a className="me-2" href="https://github.com/solm7">
+        GitHub
+      </a>
+      <a href="https://www.linkedin.com/in/solomon-dorsey-44301a209/">
+        LinkedIn
+      </a>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
 }
+
 export default Footer;
--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,4 +1,15 @@
+import myResume from "../files/SolomonDorseyResume.pdf";
 function Resume() {
-  return <h1> Resume</h1>;
+  return (
+    <div>
+      <h1> Resume</h1>
+      <iframe
+        src={myResume}
+        title="Solomon Dorsey Resume"
+        width="500"
+        height="700"
+      ></iframe>
+    </div>
+  );
 }
 export default Resume;
