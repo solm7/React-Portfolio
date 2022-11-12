3b3cd4aede1454ed99710dd390fd494bea3e1d6c
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-12 03:37:51 -0500

    Added About me

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,4 +1,13 @@
 function Home() {
-  return <h1> Home</h1>;
+  return (
+    <div>
+      <h1> About Me</h1>
+      <p className="mw-50">
+        My name is Solomon Dorsey I am a Web Developer. I graduated from the
+        UPENN LPS Full Stack coding bootcamp, and am a biologist from Loyola
+        University Maryland.
+      </p>
+    </div>
+  );
 }
 export default Home;
