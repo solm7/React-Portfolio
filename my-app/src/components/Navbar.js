8c28e006715b242f445eac959f0921b5c2cb6bd6
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-12 02:24:03 -0500

    Navbar and app.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,4 +1,49 @@
-function Navbar() {
-  return <h1> Navbar</h1>;
+import { Link } from "react-router-dom";
+
+function Navbar({ currentPage, handlePageChange }) {
+  return (
+    <div className="d-flex flex-wrap justify-content-center">
+      <nav className="navbar navbar-expand-lg">
+        <Link
+          onClick={() => handlePageChange("portfolio")}
+          className={
+            currentPage === "portfolio"
+              ? "nav-link active fs-3"
+              : "nav-link fs-3"
+          }
+          to="/"
+        >
+          Portfolio
+        </Link>
+        <Link
+          onClick={() => handlePageChange("Home")}
+          className={
+            currentPage === "Home" ? "nav-link active fs-3" : "nav-link fs-3"
+          }
+          to="/About"
+        >
+          About Me
+        </Link>
+        <Link
+          onClick={() => handlePageChange("contact")}
+          className={
+            currentPage === "contact" ? "nav-link active fs-3" : "nav-link fs-3"
+          }
+          to="/contact"
+        >
+          Contact
+        </Link>
+        <Link
+          onClick={() => handlePageChange("resume")}
+          className={
+            currentPage === "resume" ? "nav-link active fs-3" : "nav-link fs-3"
+          }
+          to="/resume"
+        >
+          Resume
+        </Link>
+      </nav>
+    </div>
+  );
 }
 export default Navbar;
