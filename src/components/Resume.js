4f81b2e6c125402ac899d0b74c7aca01630fb1c1
Solomon <solm@Solms-MacBook-Pro.local>
2024-01-19 23:33:02 -0500

    Fixed resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,4 @@
-import myResume from "/Users/solomondorsey/Desktop/BootCamp/React-Portfolio/React-Portfolio/src/components/Solomon_Dorsey_Resume.pdf";
+import myResume from "src/Solomon_Dorsey_Resume.pdf";
 function Resume() {
   return (
     <div>
