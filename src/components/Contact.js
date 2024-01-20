d443a9d1294a735b2132160ea2a46cb156b83676
Solomon <solm@Solms-MacBook-Pro.local>
2024-01-19 23:08:59 -0500

    Updated Resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <div>
       <h1> Contact Me</h1>
-      <p className="mw-50">Email: solomondorsey7@proton.me</p>
+      <p>Email: solomondorsey7@proton.me</p>
     </div>
   );
 }
