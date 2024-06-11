b004cc4230f6ccac16af69bf95a13da3bd857489
Solomon <solm@Solm-Mac.local>
2024-06-11 13:30:24 -0400

    Move resume file inside src directory

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,4 @@
-import myResume from "/Users/solomondorsey/Desktop/BootCamp/React-Portfolio/React-Portfolio/src/SolomonDorseyResume.pdf";
+import myResume from "/Users/solomondorsey/Desktop/BootCamp/React-Portfolio/React-Portfolio/src/Solomon_Dorsey_Resume_Original.docx";
 function Resume() {
   return (
     <div>
